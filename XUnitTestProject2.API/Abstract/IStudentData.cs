e19cad416e40ddfc2806d745495479b58489e544
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-10-02 04:49:28 +0530

    Add get method Student list

--- XUnitTestProject2.API/Abstract/IStudentData.cs
+++ XUnitTestProject2.API/Abstract/IStudentData.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XUnitTestProject2.API.Models;
 
 namespace XUnitTestProject2.API.Abstract
 {
     public interface IStudentData
     {
-
+        List<Student> getAllStudent();
     }
 }
--- XUnitTestProject2.API/Confreate/StudentData.cs
+++ XUnitTestProject2.API/Confreate/StudentData.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using XUnitTestProject2.API.Abstract;
+using XUnitTestProject2.API.Models;
 
 namespace XUnitTestProject2.API.Confreate
 {
-    public class StudentData:IStudentData
+    public class StudentData : IStudentData
     {
-
+        public StudentDbContext _db;
+        public StudentData(StudentDbContext db)
+        {
+            _db = db;
+        }
+        public List<Student> getAllStudent()
+        {
+            return _db.Students.ToList();
+        }
     }
 }
